9931d95188f3dd4046fa704a3afb71719791d102
suprithst28 <suprithst28@gmail.com>
2025-05-19 20:17:40 +0530

    Create main entry point

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white">
+      <h1>Welcome to Enrich Eco</h1>
+    </div>
+  )
+}
+
+export default App
